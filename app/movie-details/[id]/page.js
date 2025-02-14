e0a81f3e3352db8e0fc1057c0106193ac60ab276
Drcx-Dev <isroiljonsokievjon@mail.ru>
2025-02-14 17:36:02 +0000

    first commit

--- app/movie-details/[id]/page.js
+++ app/movie-details/[id]/page.js
@@ -1,67 +1,184 @@
 "use client";
 
-import { useParams } from "next/navigation"; // Используем useParams
+import { useParams } from "next/navigation";
 import Image from "next/image";
-import { genres } from "@/app/genres"; // Импортируем массив жанров
-import React from "react";
+import { genres } from "@/app/genres";
+import React, { useState } from "react";
 import ReactPlayer from "react-player";
+import { motion } from "framer-motion"; // Подключаем framer-motion для анимаций
 import Link from "next/link";
 
 const MovieDetails = () => {
   const { id } = useParams();
 
   if (!id) return <div>Загрузка...</div>;
-
   const genre = genres.find((genre) => genre.id === parseInt(id));
 
   if (!genre) {
     return <div>Жанр не найден!</div>;
   }
 
+  const [comments, setComments] = useState([]);
+  const [newComment, setNewComment] = useState("");
+
+  const handleAddComment = () => {
+    if (newComment.trim()) {
+      setComments([...comments, newComment]);
+      setNewComment("");
+    }
+  };
+
+  // Функция для удаления комментария
+  const handleDeleteComment = (index) => {
+    const updatedComments = comments.filter((_, i) => i !== index);
+    setComments(updatedComments);
+  };
+
+  // Функция для обработки кнопки "Поделиться"
+  const handleShare = () => {
+    if (navigator.share) {
+      navigator
+        .share({
+          title: genre.name,
+          url: window.location.href,
+        })
+        .then(() => console.log("Shared successfully")) // Если успешно поделились
+        .catch((error) => console.error("Error sharing", error)); // Обработка ошибки
+    } else {
+      navigator.clipboard
+        .writeText(window.location.href) // Копируем ссылку в буфер обмена
+        .then(() => alert("Ссылка скопирована в буфер обмена!"))
+        .catch((error) => console.error("Error copying link", error)); // Обработка ошибки
+    }
+  };
+
   return (
-    <div className="movie-details bg-gray-950 pt-8">
-      <div className="text-white text-4xl justify-self-center">
-        <h1>{genre.name}</h1>
-      </div>
-      <div className="flex flex-col lg:flex-row">
-        <Image
-          className="rounded-3xl ml-14 mb-6 lg:mb-0"
-          src={genre.image}
-          alt={genre.name}
-          width={500}
-          height={300}
-        />
+    <div className="movie-details bg-gray-950 text-white pt-8 px-4 md:px-8">
+      <motion.div
+        className="text-center mb-8"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 1 }}
+      >
+        <h1 className="text-4xl font-extrabold text-gradient">{genre.name}</h1>
+      </motion.div>
 
-        <div className="flex-none">
-          <p className="text-white text-xl pt-20 ml-5 mt-4 max-w-prose">
-            {genre.plot}
-          </p>
-        </div>
-      </div>
+      <div className="flex flex-col lg:flex-row justify-between items-start">
+        <motion.div
+          className="lg:w-1/3 mb-6 lg:mb-0"
+          initial={{ opacity: 0, x: -50 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ duration: 1, ease: "easeOut" }}
+        >
+          <Image
+            className="rounded-3xl shadow-lg transform transition-transform duration-500 hover:scale-105"
+            src={genre.image}
+            alt={genre.name}
+            width={500}
+            height={300}
+            layout="responsive"
+            objectFit="cover"
+          />
+        </motion.div>
 
-      <p className="text-white ml-14 mt-4 max-w-sm font-semibold">
-        {genre.description}
-      </p>
+        <motion.div
+          className="lg:w-2/3 text-xl max-w-prose mx-auto px-4"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 1.2 }}
+        >
+          <p className="mb-6">{genre.plot}</p>
+          <p className="font-semibold">{genre.description}</p>{" "}
+        </motion.div>
+      </div>
 
       {genre.videos && genre.videos.length > 0 ? (
-        genre.videos.map((videoUrl, index) => (
-          <div
-            key={index}
-            className="player-wrapper"
-            style={{ maxWidth: "100%", margin: "auto" }}
-          >
-            <ReactPlayer
-              url={videoUrl}
-              className="react-player"
-              width="100%"
-              height="400px"
-              controls
-            />
-          </div>
-        ))
+        <div className="mt-8">
+          {genre.videos.map((videoUrl, index) => (
+            <motion.div
+              key={index}
+              className="player-wrapper mb-6"
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ duration: 1.5 }}
+              style={{ maxWidth: "100%", margin: "auto" }}
+            >
+              <ReactPlayer
+                url={videoUrl}
+                className="react-player"
+                width="100%"
+                height="400px"
+                controls
+              />
+            </motion.div>
+          ))}
+        </div>
       ) : (
-        <p>Видео для этого жанра не найдены.</p>
+        <p className="text-center">Видео для этого жанра не найдены.</p> // Если видео нет
       )}
+
+      {/* Кнопка "Поделиться" */}
+      <motion.div
+        className="mt-7 text-center "
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 1.8 }}
+      >
+        <button
+          onClick={handleShare} // Вызов функции "Поделиться"
+          className="bg-blue-500 text-white py-3 px-6 rounded-full text-lg font-semibold hover:bg-blue-700 transition-all duration-300 transform hover:scale-105"
+        >
+          Поделиться
+        </button>
+      </motion.div>
+
+      {/* Секция комментариев */}
+      <div className="comments-section mt-12">
+        <h2 className="text-2xl font-bold mb-4">Комментарии</h2>
+
+        {/* Форма для добавления нового комментария */}
+        <textarea
+          value={newComment}
+          onChange={(e) => setNewComment(e.target.value)}
+          placeholder="Оставьте ваш комментарий..."
+          className="w-full p-4 mb-4 rounded-lg border border-gray-600 bg-gray-800 text-white"
+          rows="4"
+        />
+        <button
+          onClick={handleAddComment}
+          className="bg-blue-500 text-white py-2 px-4 rounded-full hover:bg-blue-700"
+        >
+          Оставить комментарий
+        </button>
+
+        {/* Отображение комментариев */}
+        <div className="mt-6">
+          {comments.length > 0 ? (
+            comments.map((comment, index) => (
+              <div key={index} className="bg-gray-800 p-4 mb-4 rounded-lg">
+                <p>{comment}</p>
+                {/* Кнопка для удаления комментария */}
+                <button
+                  onClick={() => handleDeleteComment(index)}
+                  className="mt-2 text-red-500 hover:text-red-700 flex justify-self-end text-2xl"
+                >
+                  X
+                </button>
+              </div>
+            ))
+          ) : (
+            <p></p>
+          )}
+        </div>
+        <footer className="bg-gray-900 text-white py-8 mt-14">
+          <div className="max-w-7xl mx-auto px-4 flex justify-between items-center">
+            <div className="text-2xl font-bold">DRCX'S MOVIES</div>
+          </div>
+          <div className="text-center text-sm text-gray-400 mt-4">
+            &copy; {new Date().getFullYear()} DRCX'S MOVIES. Все права защищены.
+          </div>
+        </footer>
+      </div>
     </div>
   );
 };
--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,10 @@
 "use client";
 import { useState, useEffect } from "react";
 import Image from "next/image";
-import avengers from "../app/photo/avengers.png";
-import avatar from "../app/photo/avatar.png";
-import titanic from "../app/photo/titanic.png";
-import us from "../app/photo/us.png";
+import avengers from "../app/photo/avengerss.jpg";
+import avatar from "../app/photo/avatar.jpg";
+import titanic from "../app/photo/titanic2.jpg";
+import us from "../app/photo/uss.jpg";
 import { genres } from "./genres"; // Импортируем список жанров
 import bg from "../app/photo/bg.png";
 import Link from "next/link";
@@ -12,8 +12,10 @@ import Link from "next/link";
 export default function Home() {
   const [searchQuery, setSearchQuery] = useState("");
   const [currentIndex, setCurrentIndex] = useState(0); // Текущее изображение
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
   const [filteredMovies, setFilteredMovies] = useState(genres); // Состояние для фильтрованных фильмов
+  const [isModalOpen, setIsModalOpen] = useState(false); // Состояние для отображения модального окна
+  const [login, setLogin] = useState(""); // Состояние для логина
+  const [password, setPassword] = useState(""); // Состояние для пароля
 
   const images = [avengers, avatar, titanic, us];
   const descriptions = [
@@ -23,16 +25,19 @@ export default function Home() {
     "Мы — психологический хоррор о борьбе человека с его зловещей тенью.",
   ];
 
+  // Фильтрация фильмов при изменении запроса
   const handleSearch = (e) => {
     e.preventDefault();
-    const filtered = genres.filter((movie) =>
-      movie.name.toLowerCase().includes(searchQuery.toLowerCase())
+    const filtered = genres.filter(
+      (movie) =>
+        searchQuery.trim() !== "" &&
+        movie.name.toLowerCase().includes(searchQuery.toLowerCase().trim())
     );
     setFilteredMovies(filtered);
   };
 
   const nextImage = () => {
-    setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length); //
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
   };
 
   const prevImage = () => {
@@ -41,129 +46,136 @@ export default function Home() {
     );
   };
 
-  // Используем useEffect для автоматического переключения слайдов через 5 секунд
   useEffect(() => {
     const interval = setInterval(() => {
       nextImage();
-    }, 5000); // Переключение через 5 секунд
+    }, 5000); // 5 сек для переклю
 
-    // Очищаем интервал, когда компонент размонтируется
     return () => clearInterval(interval);
   }, []);
 
+  const handleLogin = (e) => {
+    e.preventDefault();
+    // Здесь будет логика для входа, например, проверка данных
+    console.log("Login:", login);
+    console.log("Password:", password);
+    setIsModalOpen(false); // Закрываем модальное окно после успешного входа
+  };
+
   return (
-    <section className="bg-gray-950 min-h-screen flex flex-col">
-      <header className="bg-gray-900 text-white p-4 flex justify-between items-center w-full">
-        <div className="text-2xl font-bold">DRCX'S MOVIES</div>
+    <section className="bg-gray-950 min-h-screen flex flex-col font-sans">
+      <header className="bg-gray-900 text-white p-6 flex justify-between items-center w-full">
+        <div className="text-3xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-blue-500">
+          DRCX'S MOVIES
+        </div>
 
         <form
           onSubmit={handleSearch}
-          className="hidden md:flex items-center space-x-2"
+          className="relative flex items-center space-x-2 w-full max-w-xl"
         >
           <input
             type="text"
             value={searchQuery}
             onChange={(e) => setSearchQuery(e.target.value)}
             placeholder="Поиск фильмов..."
-            className="p-2 rounded-l-lg bg-gray-700 text-white placeholder-gray-400"
+            className="p-3 rounded-l-lg bg-gray-700 text-white placeholder-gray-400 w-full focus:ring-2 focus:ring-blue-500"
           />
           <button
             type="submit"
-            className="p-2 bg-blue-500 rounded-r-lg hover:bg-blue-600"
+            className="p-3 bg-blue-600 text-white rounded-r-lg hover:bg-blue-700 focus:ring-2 focus:ring-blue-500"
           >
             Поиск
           </button>
         </form>
 
-        <div className="hidden md:block text-lg">
-          <a href="/profile" className="hover:underline">
-            Личный кабинет
-          </a>
-        </div>
-
-        {/* Бургер-меню */}
-        <div className="md:hidden">
-          <button
-            onClick={() => setIsMenuOpen(!isMenuOpen)}
-            className="text-white p-2"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              className="w-6 h-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h16M4 18h16"
-              />
-            </svg>
-          </button>
-        </div>
-
-        {/* Меню для мобильных устройств */}
-        <div
-          className={`md:hidden ${
-            isMenuOpen ? "block" : "hidden"
-          } absolute top-0 left-0 right-0 bg-gray-900 text-white p-4`}
+        <button
+          onClick={() => setIsModalOpen(true)}
+          className="bg-blue-600 text-white p-3 rounded-lg hover:bg-blue-700 ml-4"
         >
-          <ul className="space-y-4">
-            <li>
-              <a href="/profile" className="hover:underline">
-                Личный кабинет
-              </a>
-            </li>
-            <li>
-              <form
-                onSubmit={handleSearch}
-                className="flex items-center space-x-2"
-              >
-                <input
-                  type="text"
-                  value={searchQuery}
-                  onChange={(e) => setSearchQuery(e.target.value)}
-                  placeholder="Поиск фильмов..."
-                  className="p-2 rounded-l-lg bg-gray-700 text-white placeholder-gray-400"
-                />
-                <button
-                  type="submit"
-                  className="p-2 bg-blue-500 rounded-r-lg hover:bg-blue-600"
-                >
-                  Поиск
-                </button>
-              </form>
-            </li>
-          </ul>
-        </div>
+          Войти
+        </button>
       </header>
 
-      {/* Основная часть с изображением слайдера */}
+      {/* Модальное окно для входа */}
+      {isModalOpen && (
+        <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-20">
+          <div className="bg-white p-6 rounded-lg w-96">
+            <h2 className="text-2xl font-semibold mb-4">Войти</h2>
+            <form onSubmit={handleLogin}>
+              <div className="mb-4">
+                <label htmlFor="login" className="block text-sm font-medium">
+                  Логин
+                </label>
+                <input
+                  type="text"
+                  id="login"
+                  value={login}
+                  onChange={(e) => setLogin(e.target.value)}
+                  className="w-full p-3 border border-gray-300 rounded-lg"
+                  required
+                />
+              </div>
+              <div className="mb-4">
+                <label htmlFor="password" className="block text-sm font-medium">
+                  Пароль
+                </label>
+                <input
+                  type="password"
+                  id="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  className="w-full p-3 border border-gray-300 rounded-lg"
+                  required
+                />
+              </div>
+              <button
+                type="submit"
+                className="bg-blue-600 text-white p-3 w-full rounded-lg hover:bg-blue-700"
+              >
+                Войти
+              </button>
+            </form>
+            <div className="mt-4 text-center">
+              <Link href="/register" className="text-blue-600">
+                Регистрация
+              </Link>
+            </div>
+            <button
+              onClick={() => setIsModalOpen(false)}
+              className="absolute top-2 right-2 text-white text-5xl"
+            >
+              X
+            </button>
+          </div>
+        </div>
+      )}
+
       <main className="flex justify-center items-center flex-1">
-        <div className="relative w-full h-screen">
-          <div className="transition-all duration-700 ease-in-out transform">
+        <div className="relative w-full h-[80vh] sm:h-[85vh] md:h-[90vh] lg:h-[90vh]">
+          <div
+            className="transition-all duration-700 ease-in-out transform"
+            key={currentIndex}
+          >
             <Image
               src={images[currentIndex]}
               alt="Movie poster"
-              className="rounded-lg object-cover justify-self-center w-[700px] h-[840px] mt-8"
+              className="rounded-xl w-full h-full object-cover shadow-lg"
             />
           </div>
 
-          <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 text-white text-sm text-center bg-black bg-opacity-50 p-4 rounded-lg">
+          <div className="absolute bottom-10 left-1/2 transform -translate-x-1/2 text-white text-lg sm:text-xl text-center bg-black bg-opacity-50 p-4 rounded-lg max-w-lg">
             {descriptions[currentIndex]}
           </div>
 
           <button
             onClick={prevImage}
-            className="absolute left-4 top-1/2 transform -translate-y-1/2 bg-gray-700 text-white p-4 rounded-full hover:bg-gray-600"
+            className="absolute left-8 top-1/2 transform -translate-y-1/2 bg-gray-800 text-white p-4 rounded-full hover:bg-gray-700 transition-all duration-300"
           >
             &#10094;
           </button>
           <button
             onClick={nextImage}
-            className="absolute right-4 top-1/2 transform -translate-y-1/2 bg-gray-700 text-white p-4 rounded-full hover:bg-gray-600"
+            className="absolute right-8 top-1/2 transform -translate-y-1/2 bg-gray-800 text-white p-4 rounded-full hover:bg-gray-700 transition-all duration-300"
           >
             &#10095;
           </button>
@@ -171,52 +183,47 @@ export default function Home() {
       </main>
 
       {/* Секция с кнопкой и описанием */}
-      <main>
-        <div className="justify-self-center mt-36">
-          <button className="bg-red-700 hover:bg-gray-500 rounded-lg w-44 h-12 text-white">
-            Смотреть
-          </button>
+      <div className="text-center mt-24 px-4">
+        <h1 className="text-white mt-12 text-4xl sm:text-5xl font-semibold">
+          Изучите наше разнообразие категорий
+        </h1>
+        <p className="text-gray-400 mt-4 text-lg sm:text-xl">
+          Ищете ли вы комедию, драму или что-то совершенно новое, мы найдем
+          подходящий фильм для вас.
+        </p>
+        <div className="text-white text-lg mt-10">
+          <h2>Идеальные картинки и топовое озвучивание</h2>
+          <h2 className="pt-4">
+            Фильмы и Сериалы нажмите на PLAY и станьте королём дивана
+          </h2>
         </div>
-        <div className="pl-3">
-          <h1 className="text-white mt-10 text-3xl">
-            Изучите наше разнообразие категорий:
-          </h1>
-          <p className="text-gray-500">
-            Ищете ли вы комедию, которая заставит вас смеяться, драму, которая
-            заставит задуматься, или документальный фильм, чтобы узнать что-то
-            новое.
-          </p>
-        </div>
-      </main>
+      </div>
 
-      {/* Карточки жанров */}
-      <section>
-        <div className="flex flex-wrap gap-8 mt-8 justify-center">
+      {/* Секция с фильмами */}
+      <section className="mt-16 px-4">
+        <div className="flex flex-wrap gap-8 justify-center">
           {filteredMovies.length > 0 ? (
             filteredMovies.map((product) => (
               <div
                 key={product.id}
-                className="product bg-gray-900 p-2 w-64 text-white rounded-lg shadow-md transition-transform transform hover:scale-105"
+                className="h-96 p-4 w-64 text-white rounded-xl shadow-xl transition-all transform hover:scale-105 hover:shadow-2xl"
               >
                 <Link href={`/movie-details/${product.id}`}>
-                  <div>
+                  <div className="relative">
+                    <p className="absolute top-4 right-4 px-2 py-1 rounded-lg bg-green-950 text-white">
+                      {product.grade}
+                    </p>
                     <Image
                       width={500}
-                      height={300}
+                      height={400}
                       src={product.image}
                       alt={product.name}
-                      className="w-full h-60 object-cover rounded-lg"
+                      className="w-full h-80 object-cover rounded-lg"
                     />
                     <h3 className="text-xl font-semibold mt-4">
                       {product.name}
                     </h3>
                   </div>
-                </Link>
-
-                <Link href={`/movie-details/${product.id}`}>
-                  <button className="mt-4 w-full py-2 bg-red-700 text-white rounded-full hover:bg-gray-500">
-                    Смотреть
-                  </button>
                 </Link>
               </div>
             ))
@@ -226,22 +233,12 @@ export default function Home() {
         </div>
       </section>
 
-      <section className="mt-10">
-        <div className="absolute">
-          <Image src={bg} className="w-full" alt="background" />
-        </div>
-        <h1 className="relative text-4xl text-white p-28">
-          Начните бесплатный просмотр фильма сегодня!
-        </h1>
-        <p className="relative text-2xl text-white pl-28 bottom-10">
-          Зарегистрируйтесь и смотрите бесплатно фильмы на свой вкус
-        </p>
-      </section>
-
-      {/* Footer */}
+      {/* Футер */}
       <footer className="bg-gray-900 text-white py-8 mt-14">
-        <div className="max-w-7xl mx-auto px-4 flex justify-between items-center">
-          <div className="text-2xl font-bold">DRCX'S MOVIES</div>
+        <div className="max-w-7xl mx-auto px-6 flex justify-between items-center">
+          <div className="text-3xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-blue-500">
+            DRCX'S MOVIES
+          </div>
         </div>
         <div className="text-center text-sm text-gray-400 mt-4">
           &copy; {new Date().getFullYear()} DRCX'S MOVIES. Все права защищены.
